59617a3ff594224d82bb79feb09753a567b33948
Steve Calla <callasteven@gmail.com>
2022-09-26 12:51:21 -0600

    Add weather route api handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const weatherRoutes = require("./weather-routes");
 
 router.use("/users", userRoutes);
+router.use("/weather", weatherRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//section: api/users
+
 // CREATE NEW USER
 router.post('/', async (req, res) => {
   try {
