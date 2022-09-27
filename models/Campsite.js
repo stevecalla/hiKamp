3d8eb8927c2b8fff9aae090197cc251b5ceb9323
Alex Cleveland <Alexcleveland5@gmail.com>
2022-09-27 17:27:33 -0500

    Campsite Model

--- models/Campsite.js
+++ models/Campsite.js
@@ -4,8 +4,46 @@ const sequelize = require("../config/connection");
 class Campsite extends Model { }
 
 Campsite.init(
+    {
+        code: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lat: {
+            type: DataTypes.DECIMAL,
+        },
+        lng: {
+            type: DataTypes.DECIMAL,
+        },
+        state: {
+            type: DataTypes.STRING,
+        },
+        zipCode: {
+            type: DataTypes.INTEGER,
+        },
+        city: {
+            type: DataTypes.STRING,
+        },
+        address: {
+            type: DataTypes.STRING,
+        },
+        nameState: {
+            type: DataTypes.STRING,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
 
-)
+);
 
 
 
