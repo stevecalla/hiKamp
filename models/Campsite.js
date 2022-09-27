4f48337f49a730fa0a715991b0927389f04a7e98
Alex Cleveland <Alexcleveland5@gmail.com>
2022-09-27 16:57:32 -0500

    Campsite Seed Data

---
+++ models/Campsite.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Campsite extends Model { }
+
+Campsite.init(
+
+)
+
+
+
+
+
+module.exports = Campsite;
