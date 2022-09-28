5b13de507f7ce546c7f75dfe00885aeeefd562da
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-09-27 21:34:25 -0600

    GET route basic functioning

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const weatherRoutes = require("./weather-routes");
+const campRoutes = require("./camp-routes");
 
 router.use("/users", userRoutes);
 router.use("/weather", weatherRoutes);
+router.use("/campsites", campRoutes);
 
 module.exports = router;
